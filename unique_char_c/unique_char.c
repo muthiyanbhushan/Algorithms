39182cdfade5842120d06b059882bd57033a9349
bhushan <muthiyanbhushan@gmail.com>
2017-04-12 10:57:39 -0700

    Program to determine uique characyer in c

---
+++ unique_char_c/unique_char.c
@@ -0,0 +1,29 @@
+/*
+ * unique_char.c
+ *
+ *  Created on: Feb 21, 2017
+ *      Author: bhushan
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+
+void is_unique(char* str)
+{
+	int length,i;
+	length = sizeof(str);
+	if(length > 256)
+
+	{return;}
+	for(i=0;i<length;i++)
+	{
+
+	}
+
+}
+
+void main()
+{
+
+}
